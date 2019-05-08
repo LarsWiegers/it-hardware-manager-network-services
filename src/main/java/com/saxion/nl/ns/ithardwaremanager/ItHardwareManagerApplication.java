b550f48a5db99e0ee7a7083d4eb0db5b6254e432
Lars Wiegers <larswiegers@live.nl>
2019-05-08 17:11:59 +0200

    Initial commit

---
+++ src/main/java/com/saxion/nl/ns/ithardwaremanager/ItHardwareManagerApplication.java
@@ -0,0 +1,13 @@
+package com.saxion.nl.ns.ithardwaremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHardwareManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItHardwareManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saxion/nl/ns/ithardwaremanager/ItHardwareManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saxion.nl.ns.ithardwaremanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItHardwareManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
