382b6f2799c3366c5ddbfc690ff49384bea05384
Lars Wiegers <larswiegers@live.nl>
2019-05-29 16:40:53 +0200

    Add test

--- src/main/java/com/saxion/nl/ns/ithardwaremanager/controllers/LoginController.java
+++ src/main/java/com/saxion/nl/ns/ithardwaremanager/controllers/LoginController.java
@@ -2,15 +2,8 @@ package com.saxion.nl.ns.ithardwaremanager.controllers;
 
 import com.saxion.nl.ns.ithardwaremanager.StorageContainer;
 import com.saxion.nl.ns.ithardwaremanager.contracts.StorageInterface;
-import com.saxion.nl.ns.ithardwaremanager.models.Item;
-import com.saxion.nl.ns.ithardwaremanager.models.Room;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.UUID;
-
 @Controller
 @RequestMapping("/login")
 public class LoginController {
@@ -23,7 +16,7 @@ public class LoginController {
     /**
      * ItemController
      */
-    LoginController() {
+    public LoginController() {
         // TODO use dependency injection here so we dont specify
         // TODO the real class we want but the contract we would like to use
         this.storage = StorageContainer.getStorage();
---
+++ src/test/java/com/saxion/nl/ns/ithardwaremanager/LoginControllerTest.java
@@ -0,0 +1,16 @@
+package com.saxion.nl.ns.ithardwaremanager;
+
+import com.saxion.nl.ns.ithardwaremanager.controllers.LoginController;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class LoginControllerTest {
+
+    @Test
+    public void thereIsALoginPage() {
+        Assert.assertNotEquals("", (new LoginController()).index());
+    }
+}
