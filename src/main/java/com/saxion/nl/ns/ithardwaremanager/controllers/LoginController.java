bc4357c48427005be46ebb26e7d1b57fa8e6f360
Lars Wiegers <larswiegers@live.nl>
2019-05-30 10:18:11 +0200

    login now works

--- src/main/java/com/saxion/nl/ns/ithardwaremanager/controllers/LoginController.java
+++ src/main/java/com/saxion/nl/ns/ithardwaremanager/controllers/LoginController.java
@@ -2,8 +2,12 @@ package com.saxion.nl.ns.ithardwaremanager.controllers;
 
 import com.saxion.nl.ns.ithardwaremanager.StorageContainer;
 import com.saxion.nl.ns.ithardwaremanager.contracts.StorageInterface;
+import com.saxion.nl.ns.ithardwaremanager.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/login")
 public class LoginController {
@@ -23,13 +27,36 @@ public class LoginController {
     }
 
     @GetMapping(path = "")
-    public String index() {
+    public String index(HttpSession session) {
+        if(session.getAttribute("user") != null) {
+            return "redirect:/room";
+        }
         return "login-index";
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout(HttpSession session) {
+        if(session.getAttribute("user") != null) {
+            session.removeAttribute("user");
+            return "redirect:/login";
+        }
+        return "redirect:/login";
     }
 
     @PostMapping(path = "")
     public String login(@RequestParam("email") String email,
-                        @RequestParam("password") String password) {
+                        @RequestParam("password") String password,
+                        HttpSession session) {
+        User lars = new User("lars@saxion.nl", "larsPassword");
+        User jesse = new User("jesse@saxion.nl", "jessePassword");
+        if(lars.equals(new User(email, password))) {
+            session.setAttribute("user", lars);
+        } else if(jesse.equals(new User(email, password))) {
+            session.setAttribute("user", jesse);
+        }else {
+            return "redirect:/login";
+        }
+
         return "redirect:/room";
     }
 }
